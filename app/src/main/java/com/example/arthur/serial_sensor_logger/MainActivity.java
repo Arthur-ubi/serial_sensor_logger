c9fe1d5ae6d026ef2a3e712b81df651251f7d64e
arthur-ubi <arthur@ubi.cs.ritsumei.ac.jp>
2019-06-04 19:04:58 +0900

    最新の値のみTextViewで表示

--- app/src/main/java/com/example/arthur/serial_sensor_logger/MainActivity.java
+++ app/src/main/java/com/example/arthur/serial_sensor_logger/MainActivity.java
@@ -6,16 +6,24 @@ import android.os.Bundle;
 import java.io.*;
 import android.hardware.usb.*;
 import android.util.Log;
+import android.widget.TextView;
+import android.os.Handler;
+
 import com.hoho.android.usbserial.driver.*;
 
 public class MainActivity extends AppCompatActivity {
-    
-
+    private TextView status_values;
     UsbSerialDriver usb;
+    byte buf[] = new byte[1024];
+    int num;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        status_values = (TextView)findViewById(R.id.status_value);
+
         UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
         usb = UsbSerialProber.acquire(manager);
         if (usb != null) {
@@ -30,15 +38,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void start_read_thread() {
+       
         new Thread(new Runnable() {
             public void run() {
                 try {
                     while (true) {
-                        byte buf[] = new byte[256];
-                        int num = usb.read(buf, buf.length);
+                        num = usb.read(buf, buf.length);
                         if (num > 0)
+                            runOnUiThread(new Runnable(){
+                                @Override
+                                public void run(){
+                                    status_values.setText(new String(buf, 0, num));
+                                }
+                            });
                             Log.v("arduino", new String(buf, 0, num)); // Arduinoから受信した値をlogcat出力
-                        Thread.sleep(10);
+                        Thread.sleep(10);//TODO データ欠落がないか確認(全15項目)
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
