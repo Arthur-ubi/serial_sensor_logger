c28d0755256c0e2bf80b117703be5059e3130e78
arthur-ubi <arthur@ubi.cs.ritsumei.ac.jp>
2019-06-07 00:36:48 +0900

    ログ取得開始/終了ボタンを有効化, 現在時刻をcsv保存名に使用

--- app/src/main/java/com/example/arthur/serial_sensor_logger/MainActivity.java
+++ app/src/main/java/com/example/arthur/serial_sensor_logger/MainActivity.java
@@ -14,7 +14,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.os.Handler;
 import android.widget.Toast;
 
 import java.io.File;
